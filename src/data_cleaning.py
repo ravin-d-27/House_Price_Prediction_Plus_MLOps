91ee246ae47227a558960846e0012e6cb989d440
Ravin D <ravin.d3107@outlook.com>
2023-12-17 19:51:27 +0530

    Finished Data Preprocessing  Scripts

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -4,6 +4,10 @@ import numpy as np
 import pandas as pd
 from typing import Union
 
+from sklearn.model_selection import train_test_split
+
+from pandas.core.api import Series as Series
+
 class DataStrategy(ABC):
     """
     This is the Abstract Base Class for handling data.
@@ -30,3 +34,37 @@ class DataPreprocessingStrategy(DataStrategy):
         except:
             logging.error("Failed to handle data.")
             raise Exception("Failed to handle data.")
+        
+class DataDivideStrategy(DataStrategy):
+    
+    def handle_data(self, data: pd.DataFrame) -> Union[pd.DataFrame, pd.Series]:
+        
+        try:
+            X = data.iloc[:,1:].values
+            y = data.iloc[:,0].values
+            
+            X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=1234)
+            return X_train, X_test, y_train, y_test
+        except Exception as e:
+            logging.error("Error in spliting the Data: {}".format(e))
+            raise e
+        
+
+class DataCleaning():
+    
+    def __init__(self,data:pd.DataFrame, strategy:DataStrategy) -> None:
+        self.data = data
+        self.strategy = strategy
+        
+    def handle_data(self)->Union[pd.DataFrame, pd.Series]:
+        try:
+            return self.strategy.handle_data(self.data)
+        except Exception as e:
+            logging.error("Error in Handling the Data")
+            raise e
+
+if __name__ == "__main__":
+    data = pd.read_csv('/home/ravind27/Desktop/My_Projects_and_Codes/House_Price_Prediction_Plus_MLOps/data/House_Price.csv')
+    data_cleaning = DataCleaning(data, DataPreprocessingStrategy())
+    data_cleaning.handle_data()
+    print("Done!")        
--- steps/clean_data.py
+++ steps/clean_data.py
@@ -2,8 +2,19 @@ import logging
 import pandas as pd
 from zenml import step
 
+from src.data_cleaning import DataCleaning, DataDivideStrategy, DataPreprocessingStrategy
+
+from typing_extensions import Annotated
+from typing import Tuple
+
 @step
-def clean_data(df:pd.DataFrame)->None:
+def clean_data(df:pd.DataFrame)->Tuple(
+    Annotated[pd.DataFrame, "X_train"],
+    Annotated[pd.DataFrame, "X_test"],
+    Annotated[pd.Series, "y_train"],
+    Annotated[pd.Series, "y_test"]
+    
+):
     """
     Clean data.
     
@@ -12,4 +23,16 @@ def clean_data(df:pd.DataFrame)->None:
     Returns:
         pd.DataFrame: cleaned data
     """
-    pass
+    try:
+        process_strategy = DataPreprocessingStrategy()
+        data_cleaning = DataCleaning()
+        processed_data = data_cleaning.handle_data()
+        
+        divide_strategy = DataDivideStrategy()
+        data_cleaning = DataCleaning(processed_data, divide_strategy)
+        X_train, X_test, y_train, y_test = data_cleaning.handle_data()
+        
+        logging.info("Data Cleaning Completed")
+    except Exception as e:
+        logging.error("Error in Data Cleaning: {}".format(e))
+        raise e
