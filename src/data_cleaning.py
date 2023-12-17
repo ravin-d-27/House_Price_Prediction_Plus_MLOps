a7f6a098932ca58184a59d463e86ef1aab0004d5
Ravin D <ravin.d3107@outlook.com>
2023-12-17 19:05:49 +0530

    Configured some files

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -2,7 +2,6 @@ import logging
 from abc import ABC, abstractmethod
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
 from typing import Union
 
 class DataStrategy(ABC):
@@ -13,31 +12,21 @@ class DataStrategy(ABC):
     
     @abstractmethod
     def handle_data(self, data: pd.DataFrame) -> Union[pd.DataFrame, pd.Series]:
-        """
-        This method handles data.
-        
-        Args:
-            data: raw data
-        Returns:
-            pd.DataFrame: cleaned data
-        """
         pass
     
 
 class DataPreprocessingStrategy(DataStrategy):
     
     def handle_data(self, data: pd.DataFrame)->pd.DataFrame:
-        """
-        This method handles data.
-        
-        Args:
-            data: raw data
-        Returns:
-            pd.DataFrame: cleaned data
-        """
-        
         try:
             data = data.replace({'airport':{'YES':1, 'NO':0}, 'waterbody':{'River':0, 'Lake':1, 'Lake and River':2, 'None':3}})
+            
+            data.replace({"airport": {"YES": 1, "NO": 0}, 
+                           "waterbody": {"River": 1, "Lake": 2, "Lake and River": 3, "None": 4}
+                           ,}, inplace=True)
+
+            data = data.drop(['bus_ter'],axis=1)
+            
         except:
             logging.error("Failed to handle data.")
             raise Exception("Failed to handle data.")
